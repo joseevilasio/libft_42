c1df77ecaa291875a540811a18bd0d76635bd970
joseevilasio <jose_evilasio@outlook.com>
2024-02-28 20:22:05 +0000

    refact

--- src/ft_split.c
+++ src/ft_split.c
@@ -12,6 +12,72 @@
 
 #include "libft.h"
 
+static char	*ft_ischar(char *s, char c)
+{
+	if (s == NULL)
+		return (s);
+	while (*s == c)
+		s++;
+	return (s);
+}
+
+static int	ft_strchrlen(char *s, char c)
+{
+	int	i;
+
+	i = 1;
+	s = ft_ischar(s, c);
+	if (s == NULL)
+		return (0);
+	if (*s == '\0')
+		i = 0;
+	while (*s)
+	{
+		if (*s == c)
+		{
+			s = ft_ischar(s, c);
+			if (*s != '\0')
+				i++;
+			else
+				s--;
+		}
+		s++;
+	}
+	return (i);
+}
+
+static unsigned int	ft_strlenbetweensep(char *s, char c)
+{
+	unsigned int	size;
+	char			*support;
+
+	support = s;
+	s = ft_strchr(s, c);
+	if (s == NULL)
+		size = ft_strlen(support);
+	else
+		size = ft_strlen(support) - ft_strlen(s);
+	return (size);
+}
+
+static char	*ft_strndup(char *src, unsigned int size)
+{
+	char			*str;
+	unsigned int	i;
+
+	i = 0;
+	str = (char *) malloc(size * sizeof(char) + 1);
+	if (str == NULL)
+		return (0);
+	while (i < size)
+	{
+		str[i] = src[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
 char	**ft_split(char const *s, char c)
 {
 	char	**tab;
@@ -29,11 +95,12 @@ char	**ft_split(char const *s, char c)
 	while (tab_size-- > 0)
 	{
 		ptr_s = ft_ischar(ptr_s, c);
-		size = ft_bsizechr(ptr_s, c);
-		tab[i] = (char *) malloc(size * sizeof(char) + 1);
-		if (tab[i] == NULL)
-			break ;
-		ft_strlcpy(tab[i], ptr_s, size + 1);
+		size = ft_strlenbetweensep(ptr_s, c);
+		tab[i] = ft_strndup(ptr_s, size);
+		// tab[i] = (char *) malloc(size * sizeof(char) + 1);
+		// if (tab[i] == NULL)
+		// 	break ;
+		// ft_strlcpy(tab[i], ptr_s, size + 1);
 		i++;
 		ptr_s = ptr_s + size;
 	}
