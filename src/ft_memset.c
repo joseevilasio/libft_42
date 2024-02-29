6358bd690a7e4478c3cb0f66fb5229f48589e420
joseevilasio <jose_evilasio@outlook.com>
2024-02-29 18:15:23 +0000

    delete

--- src/ft_memset.c
+++ src/ft_memset.c
@@ -6,17 +6,19 @@
 /*   By: josejunior <josejunior@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/15 17:35:02 by josejunior        #+#    #+#             */
-/*   Updated: 2024/02/15 18:27:14 by josejunior       ###   ########.fr       */
+/*   Updated: 2024/02/28 21:14:05 by josejunior       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*ft_memset(void *ptr, int c, unsigned int n)
+#include "libft.h"
+
+void	*ft_memset(void *ptr, int c, size_t n)
 {
 	unsigned int	i;
 	unsigned char	*char_ptr;
 
 	i = 0;
-	char_ptr = ptr;
+	char_ptr = (unsigned char *) ptr;
 	while (i < n)
 	{
 		char_ptr[i] = (unsigned char) c;
