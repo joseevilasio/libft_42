dc6bda5c67c66f5f1ea5d58ac0e57964f905cdca
joseevilasio <jose_evilasio@outlook.com>
2024-03-01 13:28:04 +0000

    added description

--- src/libft.h
+++ src/libft.h
@@ -6,7 +6,7 @@
 /*   By: josejunior <josejunior@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 20:05:10 by josejunior        #+#    #+#             */
-/*   Updated: 2024/03/01 10:55:36 by josejunior       ###   ########.fr       */
+/*   Updated: 2024/03/01 13:27:02 by josejunior       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,13 +62,36 @@ char			*ft_strrchr(char *s, int c);
 void			*ft_memchr(const void *s, int c, size_t n);
 int				ft_memcmp(const void *s1, const void *s2, unsigned int n);
 void			*ft_calloc(size_t nelem, size_t elsize);
+
 /* Allocates (with malloc(3)) and returns a substring from the string ’s’.
 The substring begins at index ’start’ and is of maximum size ’len’ */
 char			*ft_substr(char const *s, unsigned int start, size_t len);
+
+/* Allocates (with malloc(3)) and returns a new string, which is the result of
+the concatenation of ’s1’ and ’s2’. */
 char			*ft_strjoin(char const *s1, char const *s2);
+
+/* Allocates (with malloc(3)) and returns a copy of ’s1’ with the characters
+specified in ’set’ removed from the beginning and the end of the string. */
 // char			*ft_strtrim(char const *s1, char const *set);
+
+/* Allocates (with malloc(3)) and returns an array of strings obtained by 
+splitting ’s’ using the character ’c’ as a delimiter. The array must end with
+a NULL pointer */
 char			**ft_split(char const *s, char c);
+
+/* Allocates (with malloc(3)) and returns a string representing the integer
+received as an argument. Negative numbers must be handled. */
 char			*ft_itoa(int n);
+
+/* Applies the function ’f’ to each character of the string ’s’, and passing
+its index as first argument to create a new string (with malloc(3)) resulting
+from successive applications of ’f’. */
 char			*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+/* Applies the function ’f’ on each character of the string passed as argument,
+passing its index as first argument. Each character is passed by address to ’f’
+to be modified if necessary.*/
+void			ft_striteri(char *s, void (*f)(unsigned int, char*));
 
 #endif //LIBFT_H
